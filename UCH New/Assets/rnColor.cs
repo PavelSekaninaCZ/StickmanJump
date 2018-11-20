4f2f0cdc301252be07a41f21a55100e60d697493
Pavel Sekanina <sekeccz@gmail.com>
2018-11-20 13:39:22 +0100

    color panáček

---
+++ UCH New/Assets/rnColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rnColor : MonoBehaviour {
+    Color mycolor = Color.blue;
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
